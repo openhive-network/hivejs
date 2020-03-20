5a66aba9761884b8fc59313447aec89c01aeba82
Mahdi Yari <m.yary15@gmail.com>
2020-03-20 23:00:22 +0330

    support hive

--- src/auth/serializer/src/types.js
+++ src/auth/serializer/src/types.js
@@ -108,7 +108,7 @@ Types.asset = {
             let b_copy = b.copy(b.offset, b.offset + 7)
             symbol = new Buffer(b_copy.toBinary(), "binary").toString().replace(/\x00/g, "")
             b.skip(7)
-            // "1.000 STEEM" always written with full precision
+            // "1.000 HIVE" always written with full precision
             amount_string = fromImpliedDecimal(amount, precision)
         }
 
@@ -132,11 +132,11 @@ Types.asset = {
           {
             case "@@000000021":
               precision = 3
-              symbol = Config.get( "address_prefix" ) == "STM" ? "STEEM" : "TESTS"
+              symbol = Config.get( "address_prefix" ) == "STM" ? "HIVE" : "TESTS"
               break
             case "@@000000013":
               precision = 3
-              symbol = Config.get( "address_prefix" ) == "STM" ? "SBD" : "TBD"
+              symbol = Config.get( "address_prefix" ) == "STM" ? "HBD" : "TBD"
               break
             case "@@000000037":
               precision = 6
@@ -193,7 +193,7 @@ Types.asset = {
         return object
     },
     toObject(object, debug = {}){
-        if (debug.use_default && object === undefined) { return "0.000 STEEM"; }
+        if (debug.use_default && object === undefined) { return "0.000 HIVE"; }
         return object
     }
 }
@@ -219,9 +219,9 @@ Types.asset_symbol = {
             // Legacy Case
             let b_copy = b.copy(b.offset, b.offset + 7)
             let symbol = new Buffer(b_copy.toBinary(), "binary").toString().replace(/\x00/g, "")
-            if(symbol == "STEEM" || symbol == "TESTS")
+            if(symbol == "HIVE" || symbol == "TESTS")
               nai_string = "@@000000021"
-            else if(symbol == "SBD" || symbol == "TBD")
+            else if(symbol == "HBD" || symbol == "TBD")
               nai_string = "@@000000013"
             else if(symbol == "VESTS")
               nai_string = "@@000000037"
@@ -249,11 +249,11 @@ Types.asset_symbol = {
         {
           case "@@000000021":
             precision = 3
-              symbol = Config.get( "address_prefix" ) == "STM" ? "STEEM" : "TESTS"
+              symbol = Config.get( "address_prefix" ) == "STM" ? "HIVE" : "TESTS"
             break
           case "@@000000013":
             precision = 3
-            symbol = Config.get( "address_prefix" ) == "STM" ? "SBD" : "TBD"
+            symbol = Config.get( "address_prefix" ) == "STM" ? "HND" : "TBD"
             break
           case "@@000000037":
             precision = 6
@@ -281,7 +281,7 @@ Types.asset_symbol = {
         return object
     },
     toObject(object, debug = {}){
-        if (debug.use_default && object === undefined) { return "STEEM"; }
+        if (debug.use_default && object === undefined) { return "HIVE"; }
         return object
     }
 }
