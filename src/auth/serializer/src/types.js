b13d767a7c2c09ba2d3794926bef18f6aadd252c
feruz <fmiboy@gmail.com>
2020-04-16 06:24:00 +0300

    fix transfer issues

--- src/auth/serializer/src/types.js
+++ src/auth/serializer/src/types.js
@@ -132,11 +132,11 @@ Types.asset = {
           {
             case "@@000000021":
               precision = 3
-              symbol = Config.get( "address_prefix" ) == "STM" ? "HIVE" : "TESTS"
+              symbol = Config.get( "address_prefix" ) == "STM" ? "STEEM" : "TESTS"
               break
             case "@@000000013":
               precision = 3
-              symbol = Config.get( "address_prefix" ) == "STM" ? "HBD" : "TBD"
+              symbol = Config.get( "address_prefix" ) == "STM" ? "SBD" : "TBD"
               break
             case "@@000000037":
               precision = 6
@@ -156,6 +156,7 @@ Types.asset = {
             let res = object.split(" ")
             amount = res[0]
             symbol = res[1]
+            symbol = symbol == "HIVE" ? "STEEM" : symbol == "HBD" ? "SBD" : symbol
 
             if(symbol.startsWith("@@"))
             {
@@ -193,7 +194,7 @@ Types.asset = {
         return object
     },
     toObject(object, debug = {}){
-        if (debug.use_default && object === undefined) { return "0.000 HIVE"; }
+        if (debug.use_default && object === undefined) { return "0.000 STEEM"; }
         return object
     }
 }
@@ -251,11 +252,11 @@ Types.asset_symbol = {
         {
           case "@@000000021":
             precision = 3
-              symbol = Config.get( "address_prefix" ) == "STM" ? "HIVE" : "TESTS"
+              symbol = Config.get( "address_prefix" ) == "STM" ? "STEEM" : "TESTS"
             break
           case "@@000000013":
             precision = 3
-            symbol = Config.get( "address_prefix" ) == "STM" ? "HBD" : "TBD"
+            symbol = Config.get( "address_prefix" ) == "STM" ? "SBD" : "TBD"
             break
           case "@@000000037":
             precision = 6
